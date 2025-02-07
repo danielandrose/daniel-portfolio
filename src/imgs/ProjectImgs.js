7111b809abde75262241bfd2e7b834b1ff0bb330
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2025-02-07 21:47:35 +0530

    .

--- src/imgs/ProjectImgs.js
+++ src/imgs/ProjectImgs.js
@@ -1,6 +1,3 @@
-import Project1_img1 from "../imgs/AccPrevSys/img1.jpg"
-import Project2_img2 from "../imgs/AccPrevSys/img2.jpg"
-
 const proj2_img1 = new URL("../imgs/PlantsAndTreeInfo/proj2_img1.PNG", import.meta.url).href;
 const proj2_img2 = new URL("../imgs/PlantsAndTreeInfo/proj2_img2.PNG", import.meta.url).href;
 const proj2_img3 = new URL("../imgs/PlantsAndTreeInfo/proj2_img3.PNG", import.meta.url).href;
