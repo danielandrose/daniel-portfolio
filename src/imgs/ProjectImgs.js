db114c42aed27e2b3494582d19aac51d7fbdfb00
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2025-02-07 21:51:48 +0530

    .

--- src/imgs/ProjectImgs.js
+++ src/imgs/ProjectImgs.js
@@ -5,14 +5,14 @@ const proj2_img4 = new URL("../imgs/PlantsAndTreeInfo/proj2_img4.PNG", import.me
 const proj2_img5 = new URL("../imgs/PlantsAndTreeInfo/proj2_img5.PNG", import.meta.url).href;
 const proj2_img6 = new URL("../imgs/PlantsAndTreeInfo/proj2_img6.PNG", import.meta.url).href;
 
-const proj1_img1 = new URL("../imgs/AccPrevSys/proj1_img1.PNG", import.meta.url).href;
+const proj1_img1 = new URL("../imgs/AccPrevSys/proj1_img1.png", import.meta.url).href;
 const proj1_img2 = new URL("../imgs/AccPrevSys/proj1_img2.PNG", import.meta.url).href;
 const proj1_img3 = new URL("../imgs/AccPrevSys/proj1_img3.PNG", import.meta.url).href;
 const proj1_img4 = new URL("../imgs/AccPrevSys/proj1_img4.PNG", import.meta.url).href;
 
 const Project1=[
-    {url:proj1_img1,title:"AccPrevSys"},
-    {url:proj1_img2,title:"AccPrevSys"},
+    {url:proj2_img1,title:"AccPrevSys"},
+    {url:proj2_img2,title:"AccPrevSys"},
     {url:proj1_img3,title:"AccPrevSys"},
     {url:proj1_img4,title:"AccPrevSys"},
 ]
